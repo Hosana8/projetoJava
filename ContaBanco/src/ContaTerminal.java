9a93cde9c5e543dd9c16a77b1c184c8005204b13
hosana <gomesvitoria285@gmail.com>
2023-10-04 00:09:58 -0300

    conta terminal concluída

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,29 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) {
-        System.out.println("Olá,pessoal!");
-    }
+  public static void main(String[] args) throws Exception {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println(" Conta bancáncaria ");
+
+    System.out.println(" Por favor, insira o número da  sua conta bancária ");
+    int número = scanner.nextInt();
+
+    System.out.println(" Forneça o número da  sua agência ");
+    String agência = scanner.next();
+
+    System.out.println("Por favor, informe seu nome ");
+    String nomeCliente = scanner.next();
+
+    System.out.println("Informe o valor do seu saldo atual ");
+    double saldo = scanner.nextDouble();
+
+    System.out.println("Olá, " + nomeCliente + " obrigado por criar uma conta em nosso banco,");
+    System.out.println("sua agência é " + agência + " , conta " + número);
+    System.out.println("e seu saldo " + saldo + " já está disponível  para saque .");
+
+  }
 
 }
